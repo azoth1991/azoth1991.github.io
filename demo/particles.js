003eeae653e61e60fde6e53a9780f1a9df67006b
azoth <zhou.vip@icloud.com>
2015-12-31 14:22:59 +0800

    修改

--- demo/particles.js
+++ demo/particles.js
@@ -1524,8 +1524,10 @@ window.particlesJS = function(tag_id, params){
   canvas_el.className = pJS_canvas_class;
 
   /* set size canvas */
+
   canvas_el.style.width = "100%";
-  canvas_el.style.height = "100%";
+  canvas_el.style.height = "60%";
+  canvas_el.style.top = "20%";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
